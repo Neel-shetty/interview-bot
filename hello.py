f844ca2fb31a0f3786357eecf6666d080699c702
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-02-10 22:37:07 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from google import genai
+import os
+
+load_dotenv()
+
+def main():
+    # load the API key from the environment variable
+    API_KEY_GEMINI = os.getenv("API_KEY_GEMINI")
+    print(API_KEY_GEMINI)
+
+    client = genai.Client(api_key=API_KEY_GEMINI)
+
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash",
+        contents=["explain how Sparse Autoencoder works"],
+    )
+
+    for chunk in response:
+        print(chunk.text)
+
+if __name__ == "__main__":
+    main()
